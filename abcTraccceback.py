0389b038e2a751d15fe32dcc3542c9b66eae1cc6
gonsmoke <uouo721125@yahoo.com.tw>
2024-03-31 00:38:04 +0800

    2024/3/31

---
+++ abcTraccceback.py
@@ -0,0 +1,30 @@
+#####################第一章
+############檢查traceback內容
+
+# def a():
+#     print('Start of a()')
+#     b()  #Call b().
+
+# def b():
+#     print('Start of b()')
+#     c()  #Call c().
+
+# def c():
+#     print('Start of c()')
+#     42 / 0 #This will cause a zero divide error
+
+# a() #Call a().
+
+# def spam(number1, number2):
+#     return number1 / (number2 - 42)
+
+# spam(101, 42)
+
+# print('Hello.'
+# print('How are you?')
+
+# print(employeRecord)
+
+# 42 - "hello"
+
+print('Hello, ', name)
---
+++ zeroDivideTraceback.py
@@ -0,0 +1,11 @@
+# def spam(number1, number2):
+#     return number1 / (number2 - 42)
+
+# spam(101, 42)
+
+# print('Hello.'
+# print('How are you?')
+
+# print(employeRecord)
+
+42 - "hello"
